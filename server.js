104d7b6bb771f6588cc20854ab7356a09494a725
harshithpabbati <pabbatiharshith@gmail.com>
2019-06-04 17:55:59 +0530

    Created graphql-jwt-login

--- server.js
+++ server.js
@@ -1,24 +1,39 @@
-var express = require('express');
-var express_graphql = require('express-graphql');
-var { buildSchema } = require('graphql');
-// GraphQL schema
-var schema = buildSchema(`
-    type Query {
-        message: String
-    }
-`);
-// Root resolver
-var root = {
-    message: () => 'Hello App!'
-};
-// Create an express server and a GraphQL endpoint
-var app = express();
+import express from 'express';
+import bodyParser from 'body-parser';
+import expressGraphQL from "express-graphql";
+import cors from "cors";
+import mongoose from 'mongoose';
+import graphQLSchema from './graphql/schema';
+import graphQLResolvers from './graphql/resolvers';
+
+const app = express();
+
+app.use(
+    cors(),
+    bodyParser.json()
+)
 app.get('/', function (req, res) {
     res.send('Go to localhost:4000/graphql to see graphql :)')
   })
-app.use('/graphql', express_graphql({
-    schema: schema,
-    rootValue: root,
-    graphiql: true
-}));
-app.listen(4000, () => console.log('Express GraphQL Server Now Running On localhost:4000/graphql'));
+app.use(
+    "/graphql",
+    expressGraphQL({
+        schema: graphQLSchema,
+        rootValue: graphQLResolvers,
+        graphiql: true
+    })
+);
+
+function main() {
+    const port = process.env.PORT || 4000;
+    const URI = `mongodb://localhost/hello-app`;
+    mongoose.connect(URI, { useNewUrlParser: true })
+        .then(() => {
+            app.listen(port, () => console.log(`Server is listening on port: ${port}`));
+        })
+        .catch(err => {
+            console.log(err);
+        })
+}
+
+main();
