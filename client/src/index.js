69654d57c4cfca5ab4bc10811320ac6b73bac8fb
harshithpabbati <pabbatiharshith@gmail.com>
2019-06-14 13:45:37 +0530

    use create-react-app

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import App from './App';
+import * as serviceWorker from './serviceWorker';
 
-function renderApp() {
-  ReactDOM.render(<App />, document.getElementById('app'));
-}
+ReactDOM.render(<App />, document.getElementById('root'));
 
-renderApp();
-if (module.hot) {
-  module.hot.accept(renderApp);
-}
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
